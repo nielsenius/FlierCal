70af0d8a2c636c6f5b081bfc4010b183cad1a30e
Matthew Nielsen <mqnielsen@me.com>
2015-11-12 09:32:48 -0500

    Initial Commit

---
+++ FlierCal/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FlierCal
+//
+//  Created by Matthew Nielsen on 11/12/15.
+//  Copyright (c) 2015 Matthew Nielsen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
